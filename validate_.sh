44287ca79a65368382aba1801be8faa101445ef6
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:17:02 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,7 +0,0 @@
-file_exists "dist/basic/ckeditor.js"
-file_exists "dist/full/ckeditor.js"
-file_exists "dist/standard/ckeditor.js"
-
-file_exists "import-map.json"
-file_exists "LICENSE.md"
-file_exists "README.md"
